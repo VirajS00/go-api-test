0d06e5742cff740c6d7d3df2743d85242af27283
Viraj Shukla <virajshukla00@gmail.com>
2023-04-16 16:21:25 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-crud/controllers"
+	"github.com/go-crud/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/posts", controllers.PostsCreate)
+	r.GET("/posts", controllers.PostsGetAll)
+	r.GET("/post/:id", controllers.PostShow)
+	r.PUT("/post/:id", controllers.PostUpdate)
+	r.DELETE("/post/:id", controllers.PostDelete)
+
+	r.Run()
+}
